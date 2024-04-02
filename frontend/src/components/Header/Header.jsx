e7e7c78304a686cdb15cf53453f8ae2364dbe17d
Niaz Mohammad <85242530+niaz-cloud@users.noreply.github.com>
2024-04-03 00:50:42 +0600

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food here</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae similique fugit, eius expedita tenetur voluptatibus totam voluptate ad impedit. Inventore officiis eum error optio explicabo, mollitia dicta laborum ducimus facilis?</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
